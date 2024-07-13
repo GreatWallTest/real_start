35a56c9bad305727d7950760453b765d4eebf476
joey <714671267@qq.com>
2024-07-13 22:58:38 +0800

    基本完善了conftest 现在是肯定能执行的

--- src/real_start/conftest.py
+++ src/real_start/conftest.py
@@ -1,11 +1,15 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 import pytest
 import requests, json
 
 
 result_date = {
-        "passed":0,
-        "failed":0
+        "passed": 0,
+        "failed": 0,
+        "start": ...,
+        "api": ...,
+        "during": ...,
+        "end": ...
 }
 def pytest_configure(config: pytest.Config):
         result_date["start"] = datetime.now()
@@ -13,19 +17,21 @@ def pytest_configure(config: pytest.Config):
         # print("这是钉钉api", result_date["api"])
         
 def pytest_unconfigure():
+
         result_date["end"] = datetime.now()
 
-        result_date["during"] = result_date["end"] - result_date["start"]
-
-        send(mes_data)
-
-mes_data = f"""
-测试时间：{result_date["start"]}
-测试用例数量：{int(result_date["passed"])+int(result_date["failed"])}
-测试用时：{result_date["during"]}
+        result_date["during"] = (result_date["end"] - result_date["start"]).total_seconds()
+        rate = (result_date["passed"] / (int(result_date["passed"]) + int(result_date["failed"]))) * 100
+        mes_data = f"""通过率：{rate}%
+测试开始时间：{result_date["start"].strftime("%Y-%m-%d %H:%M:%S")}
+测试用例数量：{int(result_date["passed"]) + int(result_date["failed"])}
+测试用时：{result_date["during"]}s
 通过数量：{result_date["passed"]}
 失败数量：{result_date["failed"]}
 """
+        send(mes_data)
+
+
 def send(mes_data):
         head = {"content-type": "application/json"}
         date = {
@@ -42,10 +48,7 @@ def send(mes_data):
         print(res.text)
 
 def pytest_addoption(parser):
-    parser.addini(
-        "send_api",
-        help="钉钉发送消息地址"
-    )
+    parser.addini("send_api", help="钉钉发送消息地址")
 
 
 def pytest_report_teststatus(report):
